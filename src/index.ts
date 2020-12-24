fb77437ddb0d73ac64eb7a43f0104c399bc5dcd2
juspky <11074890+juspky@users.noreply.github.com>
2020-12-24 02:45:31 +0100

    added audio input backend

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import "./Sequelize";
 import InitAPI from "./API";
 import { StartYeelightDiscoveryService } from "./Yeelight";
 import _ from "lodash";
+import InitAudio from "./Audio";
 
 const Init = () => {
   StartYeelightDiscoveryService();
+  InitAudio();
   InitAPI();
 };
 
